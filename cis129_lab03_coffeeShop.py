68684dd394936715cfab67a0a552161ef9436583
respinoza24 <160810074+respinoza24@users.noreply.github.com>
2024-03-05 20:46:29 -0700

    Update cis129_lab03_coffeeShop.py
    Updated menu

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -27,6 +27,22 @@ numberOfMuffin = input('Enter amount of muffins: ')
 numberOfMuffin = int(numberOfMuffin)
 print(numberOfMuffin)
 
+# Number of Tea question
+print ("Number of tea bought?")
+
+# Amount of Tea displayed
+numberOfTea = input('Enter amount of tea: ')
+numberOfTea = int(numberOfTea)
+print(numberOfTea)
+
+# Number of Boba question
+print ("Number of boba bought?")
+
+# Amount of Boba displayed
+numberOfBoba = input('Enter amount of boba: ')
+numberOfBoba = int(numberOfBoba)
+print(numberOfBoba)
+
 # Second border containing *
 print ("*" * 40)
 
@@ -46,22 +62,33 @@ coffeePrice = 5.00
 
 muffinPrice = 4.00
 
+teaPrice = 3.00
+
+bobaPrice = 7.00
+
 salesTax = 1.06
 
 
 # Quantity x Price
 totalCoffeePrice = (numberOfCoffee * coffeePrice)
 totalMuffinPrice = (numberOfMuffin * muffinPrice)
-
+totalTeaPrice = (numberOfTea * teaPrice)
+totalBobaPrice = (numberOfBoba * bobaPrice)
 
 # Sales Tax
 coffeeSalesTax = (numberOfCoffee * coffeePrice * 0.06)
 muffinSalesTax = (numberOfMuffin * muffinPrice * 0.06)
+teaSalesTax = (numberOfTea * teaPrice * 0.06)
+bobaSalesTax = (numberOfBoba * bobaPrice * 0.06)
+
 totalSalesTax = (coffeeSalesTax + muffinSalesTax)
 
 # Display value and text
 print(numberOfCoffee , "Coffee at $5 each: $" , (format(totalCoffeePrice, '.2f')))
 print(numberOfMuffin , "Muffin at $4 each: $" , (format(totalMuffinPrice, '.2f')))
+print(numberOfTea , "Tea at $3 each: $" , (format(totalTeaPrice, '.2f')))
+print(numberOfBoba , "Boba at $7 each: $" , (format(totalBobaPrice, '.2f')))
+
 print("6% tax: $" , (format(totalSalesTax, '.2f')))
 
 
@@ -69,7 +96,15 @@ print("6% tax: $" , (format(totalSalesTax, '.2f')))
 print("-"*10)
 
 # Total and Tax
-totalOrder = (totalCoffeePrice + totalMuffinPrice + totalSalesTax)
+totalOrder = (totalCoffeePrice + totalMuffinPrice + totalTeaPrice + totalBobaPrice + totalSalesTax)
 
-#Total
+# Total
 print("Total: $" , (format(totalOrder, '.2f')))
+
+# End border containing *
+print ("*" * 40)
+
+# End greeting
+txt = "Thank you for shopping with us!"
+title = txt.center(41)
+print(title)
